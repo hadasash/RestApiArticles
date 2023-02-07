23f368123f4bf0dae17556981cf004bd7c3a3bff
hadasasharon@gmail.com <hadasasharon@gmail.com>
2023-02-07 12:12:42 +0200

    parse_url and explode functions to initialize URI segments

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require ("ArticleController.php");
+require ("BaseController.php");
+require ("Article.php");
+
+
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+if ((isset($uri[3]) && $uri[3] != 'article') || !isset($uri[4])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+$objFeedController = new ArticleController();
+$strMethodName = $uri[4] . 'Action';
+$objFeedController->{$strMethodName}();
+?>
